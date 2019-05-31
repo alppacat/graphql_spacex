a2fe8ec4bb2348a8cec6ca12076fcae2bc7a6e76
alppacat <queletzu@datank.ai>
2019-05-31 16:34:37 -0500

    estructura de react-app, mas logo spacex

--- gpql/src/App.js
+++ gpql/src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/spacex_logo.png';
+import styled from 'styled-components';
+
 import './App.css';
 
-function App() {
+const ContainerImage = styled.figure`
+  width: 50%;
+  height: auto;
+`
+const Image = styled.img`
+  width: 100%;
+  height: auto;
+`
+
+const App= ()=> {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ContainerImage>
+        <Image src={logo} alt="spacex logo"/>
+      </ContainerImage>
+
     </div>
   );
 }
