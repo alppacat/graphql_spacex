7e099a2cbd3703e6bc380e518a5053a06969318a
alppacat <queletzu@datank.ai>
2019-05-31 15:54:56 -0500

    Estructura basica

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true 
 }));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3010;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
